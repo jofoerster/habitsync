6eaa768fdac15af5f984143c6cf02cecade2aee5
jofoerster <foerster@hm.edu>
2025-10-29 12:17:18 +0100

    feat: remove unused percentage graph functionality

--- habitsync-api/src/main/java/de/jofoerster/habitsync/controller/HabitRecordController.java
+++ habitsync-api/src/main/java/de/jofoerster/habitsync/controller/HabitRecordController.java
@@ -58,11 +58,7 @@ public class HabitRecordController {
         if (habit == null) {
             return ResponseEntity.notFound().build();
         }
-        if (epochDayTo-epochDayFrom <= 5) { // optimize fetching for small intervals (default for list)
-            return ResponseEntity.ok(habitService.getRecordsOfHabit(habit, epochDayFrom, epochDayTo));
-        } else {
-            return ResponseEntity.ok(habitRecordService.getRecords(habit, epochDayFrom, epochDayTo));
-        }
+        return ResponseEntity.ok(habitRecordService.getRecords(habit, epochDayFrom, epochDayTo));
     }
 
     @GetMapping("/{habitUuid}/simple")
