84bdd96ee4279d9120108f0e08c8507631234e3e
jofoerster <foerster@hm.edu>
2026-01-15 16:55:19 +0100

    fix: notifications not working with postgres

--- habitsync-api/src/main/java/de/jofoerster/habitsync/config/QuartzConfig.java
+++ habitsync-api/src/main/java/de/jofoerster/habitsync/config/QuartzConfig.java
@@ -1,11 +1,13 @@
 package de.jofoerster.habitsync.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
 
 import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 public class QuartzConfig {
@@ -16,6 +18,9 @@ public class QuartzConfig {
     @Autowired
     private DataSource dataSource;
 
+    @Value("${spring.datasource.url:}")
+    private String datasourceUrl;
+
     @Bean
     public SchedulerFactoryBean schedulerFactoryBean() {
         SchedulerFactoryBean factory = new SchedulerFactoryBean();
@@ -23,6 +28,20 @@ public class QuartzConfig {
         factory.setDataSource(dataSource);
         factory.setOverwriteExistingJobs(true);
         factory.setStartupDelay(20);
+        factory.setQuartzProperties(quartzProperties());
         return factory;
+    }
+
+    private Properties quartzProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("org.quartz.jobStore.driverDelegateClass", getDriverDelegateClass());
+        return properties;
+    }
+
+    private String getDriverDelegateClass() {
+        if (datasourceUrl != null && datasourceUrl.startsWith("jdbc:postgresql")) {
+            return "org.quartz.impl.jdbcjobstore.PostgreSQLDelegate";
+        }
+        return "org.quartz.impl.jdbcjobstore.StdJDBCDelegate";
     }
 }
